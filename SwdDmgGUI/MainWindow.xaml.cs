1e1c92032415d45a752052d62c244b412467efd8
Ivor Lewis <illiated@gmail.com>
2023-12-14 16:31:47 +0000

    Add project files.

---
+++ SwdDmgGUI/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SwdDmgGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random randomRoll = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+            SwdDmgFormula swdDmg = new SwdDmgFormula(randomRoll.Next(1, 7) + randomRoll.Next(1, 7) + randomRoll.Next(1, 7));// cont page 267
+        }
+    }
+}
---
+++ SwdDmgGUI/SwdDmgFormula.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwdDmgGUI
+{
+    class SwdDmgFormula
+    {
+        private const int BASE_DAMAGE = 3;
+        private const int FLAME_DAMAGE = 2;
+
+        public SwdDmgFormula(int initialRoll)
+        {
+            roll = initialRoll;
+            CalculateDamage();
+            Debug.WriteLine($"Damage calculated after initial roll: Rolled {initialRoll} for {Damage}");
+        }
+
+        /// <summary>
+        /// Contains calculated damage.
+        /// </summary>
+        public int Damage { get; private set; }
+
+        private int roll;
+
+        /// <summary>
+        /// Sets the roll and also calculates the damage.
+        /// </summary>
+        public int Roll
+        {
+            get { return roll; }
+
+            set
+            {
+                Debug.WriteLine($"Rolled {roll}.");
+                roll = value;
+                CalculateDamage();
+            }
+        }
+
+        private bool flaming;
+
+        /// <summary>
+        /// Sets the flamimg condition and updates the damage calculation.
+        /// True if the sword id flaming.
+        /// </summary>
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                Debug.WriteLine($"flaming is {flaming}.");
+                flaming = value;
+                CalculateDamage();
+                Debug.WriteLine($"Flaming set done: Flame condition is {flaming}, Damage = {Damage}");
+            }
+        }
+
+        private bool magic;
+
+        /// <summary>
+        /// Sets the magic condition and updates the damage calculation.
+        /// True of the sword is magic.
+        /// </summary>
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                Debug.WriteLine($"magic is {magic}.");
+                magic = value;
+                CalculateDamage();
+                Debug.WriteLine($"Magic set done: Magic condition is {Magic}, Damage = {Damage}");
+            }
+        }
+
+
+        private void CalculateDamage()
+        {
+            decimal magicMultiplier = 1;
+            if (Magic)
+            {
+                magicMultiplier = 1.75M;
+            }
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            if (Flaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+
+
+        }
+
+    }
+}
+
